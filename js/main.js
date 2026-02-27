9cf7050ede94cb0e2b54140d88e4a01c9ca0a18f
T1ramisu <tpcc.general@gmail.com>
2026-02-28 00:02:10 +0700

    feat: use Choices JS for dropdown

--- js/main.js
+++ js/main.js
@@ -3,3 +3,4 @@ import "./modules/nav-height.js";
 import "./modules/tabs.js";
 import "./modules/accordion.js";
 import "./modules/nav-mobile.js";
+import "./modules/choices-init.js";
---
+++ js/modules/choices-init.js
@@ -0,0 +1,11 @@
+new Choices("#currency", {
+  searchEnabled: false,
+  shouldSort: false,
+  itemSelectText: "",
+});
+
+new Choices("#reason", {
+  searchEnabled: false,
+  shouldSort: false,
+  itemSelectText: "",
+});
